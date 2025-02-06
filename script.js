4fd4883ef3c9d4c6196e466cb5479ff2e298e386
rana-shekhar <shekharrana1011@gmail.com>
2025-02-06 11:26:26 +0530

    Add smooth scrolling and mouse follower effect; include new styles and scripts

---
+++ script.js
@@ -0,0 +1,12 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+});
+
+function circleMouseFollower() {
+    window.addEventListener('mousemove', (e) => {
+
+        document.querySelector('#minicircle').style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+    });
+}
+circleMouseFollower();
